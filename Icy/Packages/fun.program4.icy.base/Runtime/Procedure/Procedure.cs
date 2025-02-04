1a4958694ea8569426d20fcc39c76fe5d4d5784d
Icy <chengshengtzss@163.com>
2025-02-04 15:06:09 +0800

    Add : Procedure.Progress

--- Icy/Packages/fun.program4.icy.base/Runtime/Procedure/Procedure.cs
+++ Icy/Packages/fun.program4.icy.base/Runtime/Procedure/Procedure.cs
@@ -1,16 +1,27 @@
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 namespace Icy.Base
 {
 	/// <summary>
-	/// 基于FSM实现的Procedure，用于用任意顺序组合Step，来做某事
+	/// 基于FSM实现的Procedure，用于用按任意顺序组合Step，然后执行
 	/// </summary>
 	public sealed class Procedure
 	{
+		/// <summary>
+		/// Procedure的名字
+		/// </summary>
 		public string Name { get; private set; }
+		/// <summary>
+		/// 获取归一化的执行进度
+		/// </summary>
+		public float Progress { get { return (_CurrStepIdx + 1) / _Steps.Count; } }
+		/// <summary>
+		/// 内嵌的状态机
+		/// </summary>
 		private FSM _FSM;
+		/// <summary>
+		/// 所有步骤
+		/// </summary>
 		private List<ProcedureStep> _Steps;
 		/// <summary>
 		/// 当前执行的Step下标
@@ -25,18 +36,27 @@ namespace Icy.Base
 			_Steps = new List<ProcedureStep>();
 		}
 
+		/// <summary>
+		/// 添加一个指定步骤
+		/// </summary>
 		public void AddStep(ProcedureStep step)
 		{
 			_FSM.AddState(step, _Steps.Count == 0);
 			_Steps.Add(step);
 		}
 
+		/// <summary>
+		/// 启动Procedure，开始执行
+		/// </summary>
 		public void Start()
 		{
 			_CurrStepIdx = 0;
 			_FSM.Start();
 		}
 
+		/// <summary>
+		/// 执行下一步
+		/// </summary>
 		public void NextStep()
 		{
 			_CurrStepIdx++;
