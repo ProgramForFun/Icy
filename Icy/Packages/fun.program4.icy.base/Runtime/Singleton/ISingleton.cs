31d339c1a8f8135a164e81147780e116c8a80fc7
Icy <chengshengtzss@163.com>
2025-02-04 15:21:15 +0800

    Add : Singleton

---
+++ Icy/Packages/fun.program4.icy.base/Runtime/Singleton/ISingleton.cs
@@ -0,0 +1,17 @@
+
+namespace Icy.Base
+{
+
+    /// <summary>
+    /// The singleton interface.
+    /// </summary>
+    public interface ISingleton
+    {
+
+        public void InitializeSingleton();
+
+        public void ClearSingleton();
+
+    }
+
+}
