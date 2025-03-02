34e5350052114d2c87df48e665b588b765a62a00
Icy <chengshengtzss@163.com>
2025-03-02 13:09:34 +0800

    Add : namespace； TcpSession的Send改为指定偏移和长度； 接收时的try catch改为不包含OnReceive事件，否则外部的异常也会在TcpSession里被捕获

--- Icy/Packages/fun.program4.icy.network/Runtime/Tcp/TcpSession.cs
+++ Icy/Packages/fun.program4.icy.network/Runtime/Tcp/TcpSession.cs
@@ -4,224 +4,224 @@ using System;
 using Icy.Base;
 using Cysharp.Threading.Tasks;
 
-/// <summary>
-/// 一个简单的TCP通信类，基于System.Net.Sockets.TcpClient封装；
-/// 处理了粘包，基于包头的一个int大小的消息长度实现
-/// </summary>
-public class TcpSession : IDisposable
+namespace Icy.Network
 {
 	/// <summary>
-	/// 服务器域名或IP地址
+	/// 一个简单的TCP通信类，基于System.Net.Sockets.TcpClient封装；
+	/// 处理了粘包，基于包头的一个int大小的消息长度实现
 	/// </summary>
-	public string Host { get; protected set; }
-	/// <summary>
-	/// 服务器端口
-	/// </summary>
-	public int Port { get; protected set; }
-	/// <summary>
-	/// 是否已和服务器建立连接
-	/// </summary>
-	public bool IsConnected { get; private set; }
-
-	/// <summary>
-	/// 和服务器建立连接、可以通信了的事件
-	/// </summary>
-	public event Action OnConnected;
-	/// <summary>
-	/// 和服务器断开连接的事件
-	/// </summary>
-	public event Action OnDisconnected;
-	/// <summary>
-	/// 接收到服务器消息的事件
-	/// </summary>
-	public event Action<byte[], int, int> OnReceive;
-	/// <summary>
-	/// 连接服务器的异常
-	/// </summary>
-	public event Action<Exception> OnConnectException;
-	/// <summary>
-	/// 接收处理服务器数据的异常
-	/// </summary>
-	public event Action<Exception> OnHandleReceivedException;
-
-
-	protected TcpClient _TcpClient;
-	protected NetworkStream _Stream;
-	protected byte[] _ReceiveBuffer;
-	protected byte[] _SendBuffer;
-
-	/// <summary>
-	/// 当前正在接收的消息的长度，包括长度本身的4字节
-	/// </summary>
-	protected int _CurMsgLen = 0;
-	/// <summary>
-	/// 当前正在接收的消息（包括消息长度），在buffer中的起始index
-	/// </summary>
-	protected int _CurMsgStartIdx = 0;
-	/// <summary>
-	/// 当前Buffer中剩余多少数据
-	/// </summary>
-	protected int _BufferRemain = 0;
-	/// <summary>
-	/// Buffer大小
-	/// </summary>
-	protected int _BufferSize;
-	/// <summary>
-	/// 消息长度本身，的长度
-	/// </summary>
-	protected const int MSG_LENGTH_SIZE = sizeof(int);
-
-
-	public TcpSession(string host, int port, int bufferSize = 4096)
+	public class TcpSession : IDisposable
 	{
-		Host = host;
-		Port = port;
-		_BufferSize = bufferSize;
-		_ReceiveBuffer = new byte[bufferSize];
-		_SendBuffer = new byte[bufferSize];
-	}
+		/// <summary>
+		/// 服务器域名或IP地址
+		/// </summary>
+		public string Host { get; protected set; }
+		/// <summary>
+		/// 服务器端口
+		/// </summary>
+		public int Port { get; protected set; }
+		/// <summary>
+		/// 是否已和服务器建立连接
+		/// </summary>
+		public bool IsConnected { get; private set; }
 
-	/// <summary>
-	/// 和服务器建立连接
-	/// </summary>
-	public async UniTask Connect()
-	{
-		if (IsConnected)
+		/// <summary>
+		/// 和服务器建立连接、可以通信了的事件
+		/// </summary>
+		public event Action OnConnected;
+		/// <summary>
+		/// 和服务器断开连接的事件
+		/// </summary>
+		public event Action OnDisconnected;
+		/// <summary>
+		/// 接收到服务器消息的事件
+		/// </summary>
+		public event Action<byte[], int, int> OnReceive;
+		/// <summary>
+		/// 连接服务器的异常
+		/// </summary>
+		public event Action<Exception> OnConnectException;
+		/// <summary>
+		/// 接收处理服务器数据的异常
+		/// </summary>
+		public event Action<Exception> OnListenException;
+
+
+		protected TcpClient _TcpClient;
+		protected NetworkStream _Stream;
+		protected byte[] _ReceiveBuffer;
+		protected byte[] _SendBuffer;
+
+		/// <summary>
+		/// 当前正在接收的消息的长度，包括长度本身的4字节
+		/// </summary>
+		protected int _CurMsgLen = 0;
+		/// <summary>
+		/// 当前正在接收的消息（包括消息长度），在buffer中的起始index
+		/// </summary>
+		protected int _CurMsgStartIdx = 0;
+		/// <summary>
+		/// 当前Buffer中剩余多少数据
+		/// </summary>
+		protected int _BufferRemain = 0;
+		/// <summary>
+		/// Buffer大小
+		/// </summary>
+		protected int _BufferSize;
+		/// <summary>
+		/// 消息长度本身，的长度
+		/// </summary>
+		protected const int MSG_LENGTH_SIZE = sizeof(int);
+
+
+		public TcpSession(string host, int port, int bufferSize = 4096)
 		{
-			Log.LogError("Duplicate Connect", "TcpSession");
-			return;
+			Host = host;
+			Port = port;
+			_BufferSize = bufferSize;
+			_ReceiveBuffer = new byte[bufferSize];
+			_SendBuffer = new byte[bufferSize];
 		}
 
-		try
+		/// <summary>
+		/// 和服务器建立连接
+		/// </summary>
+		public async UniTask Connect()
 		{
-			_TcpClient = new TcpClient();
-			await _TcpClient.ConnectAsync(Host, Port);
-			_TcpClient.NoDelay = true;  //关闭Nagle算法
-			_Stream = _TcpClient.GetStream();
-			IsConnected = true;
-			OnConnected?.Invoke();
-			Log.LogInfo("Connected", "TcpSession");
-		}
-		catch (Exception ex)
-		{
-			Log.LogError($"Connect exception : {ex}", "TcpSession");
-			OnConnectException?.Invoke(ex);
-		}
-	}
+			if (IsConnected)
+			{
+				Log.LogError("Duplicate Connect", "TcpSession");
+				return;
+			}
 
-	/// <summary>
-	/// 监听消息
-	/// </summary>
-	public async UniTask Listen()
-	{
-		Log.LogInfo("Start Listen", "TcpSession");
-		try
+			try
+			{
+				_TcpClient = new TcpClient();
+				await _TcpClient.ConnectAsync(Host, Port);
+				_TcpClient.NoDelay = true;  //关闭Nagle算法
+				_Stream = _TcpClient.GetStream();
+				IsConnected = true;
+				OnConnected?.Invoke();
+				Log.LogInfo("Connected", "TcpSession");
+			}
+			catch (Exception ex)
+			{
+				Log.LogError($"Connect exception : {ex}", "TcpSession");
+				OnConnectException?.Invoke(ex);
+			}
+		}
+
+		/// <summary>
+		/// 监听消息
+		/// </summary>
+		public async UniTask Listen()
 		{
+			Log.LogInfo("Start Listen", "TcpSession");
 			while (IsConnected)
 			{
-				int receivedSize = await _Stream.ReadAsync(_ReceiveBuffer, _BufferRemain, _BufferSize - _BufferRemain);
+				int receivedSize = 0;
+				try
+				{
+					receivedSize = await _Stream.ReadAsync(_ReceiveBuffer, _BufferRemain, _BufferSize - _BufferRemain);
+				}
+				catch (Exception ex)
+				{
+					Log.LogError($"Listen exception : {ex}", "TcpSession");
+					OnListenException?.Invoke(ex);
+				}
 				HandleReceived(_ReceiveBuffer, receivedSize);
 			}
 		}
-		catch (Exception ex)
+
+		/// <summary>
+		/// 发送消息
+		/// </summary>
+		public virtual void Send(byte[] msg, int startIdx, int count)
 		{
-			Log.LogError($"HandleReceived exception : {ex}", "TcpSession");
-			OnHandleReceivedException?.Invoke(ex);
-		}
-	}
-
-	/// <summary>
-	/// 发送消息
-	/// </summary>
-	public void Send(string msg)
-	{
-		Send(Encoding.UTF8.GetBytes(msg));
-	}
-	public void Send(byte[] msg)
-	{
-		if (!IsConnected)
-		{
-			Log.LogError("Trying to send when disconnected", "TcpSession");
-			return;
-		}
-
-		//消息结构前4个byte为本数据包的长度，以解决粘包问题
-		//  消息长度 消息本体
-		// |- - - -| - - -...
-		//     4      n
-
-		//1、长度 = 消息ID + 消息本体的长度
-		int len = msg.Length + MSG_LENGTH_SIZE;
-		byte[] buf = BitConverter.GetBytes(len);
-		Array.Copy(buf, 0, _SendBuffer, 0, buf.Length);
-		//2、消息本体
-		Array.Copy(msg, 0, _SendBuffer, MSG_LENGTH_SIZE, msg.Length);
-		_Stream.Write(_SendBuffer, 0, len);
-	}
-
-	/// <summary>
-	/// 关闭连接
-	/// </summary>
-	public void Close()
-	{
-		if (!IsConnected)
-		{
-			Log.LogError("Close when disconnected", "TcpSession");
-			return;
-		}
-		IsConnected = false;
-		_TcpClient.Close();
-		_TcpClient = null;
-		OnDisconnected?.Invoke();
-		Log.LogInfo("Close", "TcpSession");
-	}
-
-	/// <summary>
-	/// 处理TCP接收到的消息
-	/// </summary>
-	private void HandleReceived(byte[] buffer, int receivedSize)
-	{
-		_BufferRemain += receivedSize;
-		while (true)
-		{
-			if (_CurMsgLen == 0)
+			if (!IsConnected)
 			{
-				//处理消息长度都没能完整发过来的情况
-				if (_BufferRemain < MSG_LENGTH_SIZE)
-					break;
-				//获取消息长度
-				else
-					_CurMsgLen = BitConverter.ToInt32(buffer, _CurMsgStartIdx);
+				Log.LogError("Trying to send when disconnected", "TcpSession");
+				return;
 			}
 
-			//当前buffer里的数据，足够一条完整消息，正常处理
-			if (_BufferRemain >= _CurMsgLen)
-			{
-				OnReceive?.Invoke(buffer, _CurMsgStartIdx + MSG_LENGTH_SIZE, _CurMsgLen - MSG_LENGTH_SIZE);
-				_BufferRemain -= _CurMsgLen;
-				if (_BufferRemain > 0)
-					_CurMsgStartIdx += _CurMsgLen;
-				_CurMsgLen = 0;
+			//消息结构前4个byte为本数据包的长度，以解决粘包问题
+			//  消息长度 消息本体
+			// |- - - -| - - -...
+			//     4      n
 
-				//如果当前buffer有剩余 且 不是从下标0开始的，挪到下标0开始，方便逻辑处理
-				//TODO：其实应该搞一个环形buffer，避免Copy，更高效
-				if (_BufferRemain > 0 && _CurMsgStartIdx > 0)
+			//1、长度 = 消息ID + 消息本体的长度
+			int len = count + MSG_LENGTH_SIZE;
+			byte[] buf = BitConverter.GetBytes(len);
+			Array.Copy(buf, 0, _SendBuffer, 0, buf.Length);
+			//2、消息本体
+			Array.Copy(msg, 0, _SendBuffer, MSG_LENGTH_SIZE, count);
+			_Stream.Write(_SendBuffer, 0, len);
+		}
+
+		/// <summary>
+		/// 关闭连接
+		/// </summary>
+		public void Close()
+		{
+			if (!IsConnected)
+			{
+				Log.LogError("Close when disconnected", "TcpSession");
+				return;
+			}
+			IsConnected = false;
+			_TcpClient.Close();
+			_TcpClient = null;
+			OnDisconnected?.Invoke();
+			Log.LogInfo("Close", "TcpSession");
+		}
+
+		/// <summary>
+		/// 处理TCP接收到的消息
+		/// </summary>
+		private void HandleReceived(byte[] buffer, int receivedSize)
+		{
+			_BufferRemain += receivedSize;
+			while (true)
+			{
+				if (_CurMsgLen == 0)
 				{
-					//Log.LogInfo("Move " + _BufferRemain);
-					Array.Copy(_ReceiveBuffer, _CurMsgStartIdx, _ReceiveBuffer, 0, _BufferRemain);
-					_CurMsgStartIdx = 0;
+					//处理消息长度都没能完整发过来的情况
+					if (_BufferRemain < MSG_LENGTH_SIZE)
+						break;
+					//获取消息长度
+					else
+						_CurMsgLen = BitConverter.ToInt32(buffer, _CurMsgStartIdx);
 				}
-			}
-			//等待下一轮数据到来，凑成完整消息再处理
-			else
-				break;
-		}
-	}
 
-	public void Dispose()
-	{
-		if (IsConnected)
-			Close();
-		Log.LogInfo("Dispose", "TcpSession");
+				//当前buffer里的数据，足够一条完整消息，正常处理
+				if (_BufferRemain >= _CurMsgLen)
+				{
+					OnReceive?.Invoke(buffer, _CurMsgStartIdx + MSG_LENGTH_SIZE, _CurMsgLen - MSG_LENGTH_SIZE);
+					_BufferRemain -= _CurMsgLen;
+					if (_BufferRemain > 0)
+						_CurMsgStartIdx += _CurMsgLen;
+					_CurMsgLen = 0;
+
+					//如果当前buffer有剩余 且 不是从下标0开始的，挪到下标0开始，方便逻辑处理
+					//TODO：其实应该搞一个环形buffer，避免Copy，更高效
+					if (_BufferRemain > 0 && _CurMsgStartIdx > 0)
+					{
+						//Log.LogInfo("Move " + _BufferRemain);
+						Array.Copy(_ReceiveBuffer, _CurMsgStartIdx, _ReceiveBuffer, 0, _BufferRemain);
+						_CurMsgStartIdx = 0;
+					}
+				}
+				//等待下一轮数据到来，凑成完整消息再处理
+				else
+					break;
+			}
+		}
+
+		public virtual void Dispose()
+		{
+			if (IsConnected)
+				Close();
+			Log.LogInfo("Dispose", "TcpSession");
+		}
 	}
 }
