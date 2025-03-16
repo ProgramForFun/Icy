1f5454b89c8d4397c13a03192108d50a09dd28f1
Icy <chengshengtzss@163.com>
2025-03-16 16:16:35 +0800

    Add : namespace

--- Icy/Packages/fun.program4.icy.ui/Runtime/Component/Empty4RaycastTarget.cs
+++ Icy/Packages/fun.program4.icy.ui/Runtime/Component/Empty4RaycastTarget.cs
@@ -1,21 +1,24 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-/// <summary>
-/// 使用alpha为0的Image实现不可见的可交互区域，会导致Overdraw；
-/// 因为UGUI仍会去渲染alpha为0的控件；
-/// 因此实现一个只在逻辑上响应Raycast但是不参与绘制的组件，降低Fill Rate
-/// </summary>
-[RequireComponent(typeof(CanvasRenderer))]
-public class Empty4RaycastTarget : MaskableGraphic
+namespace Icy.UI
 {
-    protected Empty4RaycastTarget()
-    {
-        useLegacyMeshGeneration = false;
-    }
+	/// <summary>
+	/// 使用alpha为0的Image实现不可见的可交互区域，会导致Overdraw；
+	/// 因为UGUI仍会去渲染alpha为0的控件；
+	/// 因此实现一个只在逻辑上响应Raycast但是不参与绘制的组件，降低Fill Rate
+	/// </summary>
+	[RequireComponent(typeof(CanvasRenderer))]
+	public class Empty4RaycastTarget : MaskableGraphic
+	{
+		protected Empty4RaycastTarget()
+		{
+			useLegacyMeshGeneration = false;
+		}
 
-    protected override void OnPopulateMesh(VertexHelper toFill)
-    {
-        toFill.Clear();
-    }
+		protected override void OnPopulateMesh(VertexHelper toFill)
+		{
+			toFill.Clear();
+		}
+	}
 }
