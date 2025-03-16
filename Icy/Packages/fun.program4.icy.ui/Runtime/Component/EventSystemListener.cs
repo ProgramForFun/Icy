0b3a9642a62e14cdbee871a31cef43cff26b1add
Icy <chengshengtzss@163.com>
2025-03-16 15:47:21 +0800

    Add : 实用基础UI组件

---
+++ Icy/Packages/fun.program4.icy.ui/Runtime/Component/EventSystemListener.cs
@@ -0,0 +1,168 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// 物体的EventSystem事件监听
+/// </summary>
+public class EventSystemListener : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
+								, IPointerExitHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler, IPointerEnterHandler
+{
+	public Action<PointerEventData> OnClick;
+	public Action<PointerEventData> OnClickDown;
+	public Action<PointerEventData> OnClickUp;
+	public Action<PointerEventData> OnSlideIn;
+	public Action<PointerEventData> OnSlideOut;
+	public Action<PointerEventData> OnBeginDragObj;
+	public Action<PointerEventData> OnEndDragObj;
+	public Action<PointerEventData> OnDragObj;
+	public Action<PointerEventData> OnDropObj;
+
+	private bool _isSlideOut;
+	//private bool _isSlideIn;
+	private bool _isDragging = false;
+
+	private int _FingerId = 0;
+
+	[Header("是否启用点击CD")]
+	public bool ClickCD = false;
+	public float ClickCDTime = 1.2f;
+	private float _ClickCDTimer = 0;
+	private bool _CanClick = true;
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		//双指以上同时操作时屏蔽点击事件
+#if !(UNITY_EDITOR || UNITY_STANDALONE_WIN)
+		if (Input.touchCount > 1)
+		{
+			return;
+		}
+#endif
+
+		//PC平台只响应鼠标左键
+#if UNITY_STANDALONE
+		if (eventData != null && eventData.button != PointerEventData.InputButton.Left)
+			return;
+#endif
+
+		_isSlideOut = false;
+		if (!_isSlideOut && _CanClick)
+		{
+			OnClick?.Invoke(eventData);
+			// 开启了点击CD
+			if (ClickCD)
+			{
+				_ClickCDTimer = 0;
+				_CanClick = false;
+			}
+		}
+	}
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		// 不是当前的手指 不响应
+		if (_FingerId != eventData.pointerId)
+			return;
+		//_isSlideIn = true;
+		OnSlideIn?.Invoke(eventData);
+	}
+
+	public void OnPointerDown(PointerEventData eventData)
+	{
+		//PC平台只响应鼠标左键
+#if UNITY_STANDALONE
+		if (eventData.button != PointerEventData.InputButton.Left)
+			return;
+#endif
+		_FingerId = eventData.pointerId;
+		_isSlideOut = false;
+		OnClickDown?.Invoke(eventData);
+	}
+
+	public void OnPointerUp(PointerEventData eventData)
+	{
+		//PC平台只响应鼠标左键
+#if UNITY_STANDALONE
+		if (eventData.button != PointerEventData.InputButton.Left)
+			return;
+#endif
+		// 不是当前的手指 不响应
+		if (_FingerId != eventData.pointerId)
+			return;
+
+		// 拖拽的时候 结束时刻 先响应Up再响应DragEnd，所以拖拽下 留给DragEnd把ID设置默认
+		if (!_isDragging)
+			_FingerId = int.MinValue;
+
+		OnClickUp?.Invoke(eventData);
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		// 不是当前的手指 不响应
+		if (_FingerId != eventData.pointerId)
+			return;
+		_isSlideOut = true;
+		OnSlideOut?.Invoke(eventData);
+	}
+
+	public void OnBeginDrag(PointerEventData eventData)
+	{
+		//PC平台只响应鼠标左键
+#if UNITY_STANDALONE
+		if (eventData.button != PointerEventData.InputButton.Left)
+			return;
+#endif
+
+		// 不是当前的手指 不响应
+		if (_FingerId != eventData.pointerId)
+			return;
+
+		_isDragging = true;
+		OnBeginDragObj?.Invoke(eventData);
+	}
+
+	public void OnEndDrag(PointerEventData eventData)
+	{
+		// 不是当前的手指 不响应
+		if (_FingerId != eventData.pointerId)
+			return;
+
+		_FingerId = int.MinValue;
+		_isDragging = false;
+		OnEndDragObj?.Invoke(eventData);
+	}
+
+	public void OnDrag(PointerEventData eventData)
+	{
+		// 不是当前的手指 不响应
+		if (_FingerId != eventData.pointerId)
+			return;
+		OnDragObj?.Invoke(eventData);
+	}
+
+	public void OnDrop(PointerEventData eventData)
+	{
+		OnDropObj?.Invoke(eventData);
+	}
+
+	void OnEnable()
+	{
+		if (ClickCD)
+		{
+			_CanClick = true;
+			_ClickCDTimer = 0;
+		}
+	}
+
+	void Update()
+	{
+		if (ClickCD && !_CanClick)
+		{
+			_ClickCDTimer += Time.deltaTime;
+			if (_ClickCDTimer > ClickCDTime)
+				_CanClick = true;
+		}
+	}
+}
---
+++ Icy/Packages/fun.program4.icy.ui/Runtime/Component/NotchScreenAdapter.cs
@@ -0,0 +1,56 @@
+using Icy.Base;
+using UnityEngine;
+
+/// <summary>
+/// 适配刘海屏
+/// </summary>
+public class NotchScreenAdapter : MonoBehaviour
+{
+	private RectTransform _rectTrans;
+	private Rect _LastSafeArea;
+
+	protected void Start()
+	{
+		_rectTrans = GetComponent<RectTransform>();
+		Refresh();
+	}
+
+#if UNITY_EDITOR
+	//为了可以工作在editor下的Device Simulator
+	protected void Update()
+	{
+		Refresh();
+	}
+#endif
+
+	private void Refresh()
+	{
+		Rect safeArea = GetSafeArea();
+
+		if (safeArea != _LastSafeArea)
+			ApplySafeArea(safeArea);
+	}
+
+	private Rect GetSafeArea()
+	{
+		// Log.LogInfo("safe area = " + Screen.safeArea.ToString());
+		return Screen.safeArea;
+	}
+
+	private void ApplySafeArea(Rect r)
+	{
+		_LastSafeArea = r;
+
+		// Convert safe area rectangle from absolute pixels to normalised anchor coordinates
+		Vector2 anchorMin = r.position;
+		Vector2 anchorMax = r.position + r.size;
+		anchorMin.x /= Screen.width;
+		anchorMin.y /= Screen.height;
+		anchorMax.x /= Screen.width;
+		anchorMax.y /= Screen.height;
+		_rectTrans.anchorMin = anchorMin;
+		_rectTrans.anchorMax = anchorMax;
+
+		Log.LogInfo($"New safe area applied to {name}: x={r.x}, y={r.y}, w={r.width}, h={r.height} on full extents w={Screen.width}, h={Screen.height}", "NotchScreenAdapter");
+	}
+}
