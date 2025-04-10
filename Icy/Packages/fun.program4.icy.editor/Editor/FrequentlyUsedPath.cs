37009e8ccd530165324402d7e79df1bd81cbc3fc
Icy <chengshengtzss@163.com>
2025-04-10 10:45:56 +0800

    Add : namespace

--- Icy/Packages/fun.program4.icy.editor/Editor/FrequentlyUsedPath.cs
+++ Icy/Packages/fun.program4.icy.editor/Editor/FrequentlyUsedPath.cs
@@ -2,37 +2,40 @@ using System.IO;
 using UnityEditor;
 using UnityEngine;
 
-/// <summary>
-/// 把常用的路径，加入到菜单里
-/// </summary>
-public static class FrequentlyUsedPath
+namespace Icy.Editor
 {
-	[MenuItem("Icy/Path/Persistent")]
-	static void OpenPersistentDataPath()
+	/// <summary>
+	/// 把常用的路径，加入到菜单里
+	/// </summary>
+	public static class FrequentlyUsedPath
 	{
-		System.Diagnostics.Process.Start(Application.persistentDataPath);
-	}
+		[MenuItem("Icy/Path/Persistent")]
+		static void OpenPersistentDataPath()
+		{
+			System.Diagnostics.Process.Start(Application.persistentDataPath);
+		}
 
-	[MenuItem("Icy/Path/StreamingAssets")]
-	static void OpenStreamingAssetsPath()
-	{
-		if (Directory.Exists(Application.streamingAssetsPath))
-			System.Diagnostics.Process.Start(Application.streamingAssetsPath);
-		else
-			Debug.LogError($"Can not find {Application.streamingAssetsPath}");
-	}
+		[MenuItem("Icy/Path/StreamingAssets")]
+		static void OpenStreamingAssetsPath()
+		{
+			if (Directory.Exists(Application.streamingAssetsPath))
+				System.Diagnostics.Process.Start(Application.streamingAssetsPath);
+			else
+				Debug.LogError($"Can not find {Application.streamingAssetsPath}");
+		}
 
-	[MenuItem("Icy/Path/EditorLog")]
-	static void OpenEditorLogPath()
-	{
-		string editorLogPath;
+		[MenuItem("Icy/Path/EditorLog")]
+		static void OpenEditorLogPath()
+		{
+			string editorLogPath;
 #if UNITY_EDITOR_WIN
-		string userDir = System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData);
-		editorLogPath = Path.Combine(userDir, "Unity", "Editor").Replace("\\", "/");
+			string userDir = System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData);
+			editorLogPath = Path.Combine(userDir, "Unity", "Editor").Replace("\\", "/");
 #else
 		Debug.LogError("OpenEditorLogPath can only available on Windows as present");
 #endif
-		if (!string.IsNullOrEmpty(editorLogPath))
-			System.Diagnostics.Process.Start(editorLogPath);
+			if (!string.IsNullOrEmpty(editorLogPath))
+				System.Diagnostics.Process.Start(editorLogPath);
+		}
 	}
 }
