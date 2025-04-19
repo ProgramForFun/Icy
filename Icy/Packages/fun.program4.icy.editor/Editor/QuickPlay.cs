6adbcaf7431e6b9214095d0392e162859ea58484
Icy <chengshengtzss@163.com>
2025-04-19 16:21:44 +0800

    Add : 快速进入Editor Play状态的按钮

---
+++ Icy/Packages/fun.program4.icy.editor/Editor/QuickPlay.cs
@@ -0,0 +1,62 @@
+using UnityEditor;
+using UnityEngine;
+using UnityToolbarExtender;
+
+namespace Icy.Editor
+{
+	/// <summary>
+	/// 不 进行Domain Reload，直接进入Play状态；
+	/// 如果你不知道这个功能的原理和限制，就不要使用这个功能
+	/// </summary>
+	[InitializeOnLoad]
+	public class QuickPlayViewer
+	{
+		static QuickPlayViewer()
+		{
+			ToolbarExtender.RightToolbarGUI.Add(OnToolbarGUI);
+			EditorApplication.playModeStateChanged += ClearPlayMode;
+		}
+
+		static void OnToolbarGUI()
+		{
+			if (EditorApplication.isPlaying)
+				return;
+
+			Color originalBackgroundColor = GUI.backgroundColor;
+			GUI.backgroundColor = new Color(180.0f / 255.0f, 180.0f / 255.0f, 180.0f / 255.0f);
+
+			GUIStyle style = new GUIStyle("Button")
+			{
+				fontSize = 13,
+				alignment = TextAnchor.MiddleCenter,
+				imagePosition = ImagePosition.ImageAbove,
+				fontStyle = FontStyle.Normal,
+				fixedWidth = 70
+			};
+
+			if (GUILayout.Button(new GUIContent("QuickPlay", "快速进入Play状态，如果你不知道这个功能的原理和限制，就不要使用这个功能"), style))
+				QuickPlay();
+
+			GUI.backgroundColor = originalBackgroundColor;
+		}
+
+		static void QuickPlay()
+		{
+			if (EditorApplication.isPlaying == false)
+			{
+				EditorSettings.enterPlayModeOptionsEnabled = true;
+				EditorSettings.enterPlayModeOptions = EnterPlayModeOptions.DisableDomainReload;
+				EditorApplication.EnterPlaymode();
+			}
+		}
+
+		static void ClearPlayMode(PlayModeStateChange state)
+		{
+			if (state == PlayModeStateChange.ExitingPlayMode)
+			{
+				if (EditorSettings.enterPlayModeOptionsEnabled)
+					EditorSettings.enterPlayModeOptionsEnabled = false;
+			}
+		}
+	}
+}
