08517b1c9a4557f76f2c791b35756815d1edf4e5
Icy <chengshengtzss@163.com>
2025-04-28 09:25:38 +0800

    Add ：namespace

--- Icy/Packages/fun.program4.icy.ui/Runtime/DataBinding/UguiBindManager.cs
+++ Icy/Packages/fun.program4.icy.ui/Runtime/DataBinding/UguiBindManager.cs
@@ -1,68 +1,71 @@
 using Icy.Base;
 using System.Collections.Generic;
 
-/// <summary>
-/// 管理UI组件bind数据的映射关系
-/// </summary>
-internal sealed class UguiBindManager : Singleton<UguiBindManager>
+namespace Icy.UI
 {
 	/// <summary>
-	/// Ugui组件
+	/// 管理UI组件bind数据的映射关系
 	/// </summary>
-	private List<object> _UguiCompList;
-	/// <summary>
-	/// Bind的数据
-	/// </summary>
-	private List<object> _BindableList;
-	/// <summary>
-	/// 监听数据变化的listener
-	/// </summary>
-	private List<object> _ListenerList;
-
-	protected override void OnInitialized()
+	internal sealed class UguiBindManager : Singleton<UguiBindManager>
 	{
-		base.OnInitialized();
-		_UguiCompList = new List<object>();
-		_BindableList = new List<object>();
-		_ListenerList = new List<object>();
-	}
+		/// <summary>
+		/// Ugui组件
+		/// </summary>
+		private List<object> _UguiCompList;
+		/// <summary>
+		/// Bind的数据
+		/// </summary>
+		private List<object> _BindableList;
+		/// <summary>
+		/// 监听数据变化的listener
+		/// </summary>
+		private List<object> _ListenerList;
 
-	/// <summary>
-	/// 指定的Ugui组件和数据，是否已经Bind过了
-	/// </summary>
-	internal bool AlreadyBinded(object uguiComp, object bindableData)
-	{
-		bool contains = _UguiCompList.Contains(uguiComp) && _BindableList.Contains(bindableData);
-		return contains;
-	}
-
-	/// <summary>
-	/// Bind指定的Ugui组件和数据
-	/// </summary>
-	internal void Bind(object uguiComp, object bindableData, object listener)
-	{
-		if (!AlreadyBinded(uguiComp, bindableData))
+		protected override void OnInitialized()
 		{
-			_UguiCompList.Add(uguiComp);
-			_BindableList.Add(bindableData);
-			_ListenerList.Add(listener);
+			base.OnInitialized();
+			_UguiCompList = new List<object>();
+			_BindableList = new List<object>();
+			_ListenerList = new List<object>();
 		}
-	}
 
-	/// <summary>
-	/// 解除Bind指定的Ugui组件和数据
-	/// </summary>
-	internal object Unbind(object uguiComp, object bindableData)
-	{
-		if (AlreadyBinded(uguiComp, bindableData))
+		/// <summary>
+		/// 指定的Ugui组件和数据，是否已经Bind过了
+		/// </summary>
+		internal bool AlreadyBinded(object uguiComp, object bindableData)
 		{
-			int idx = _UguiCompList.IndexOf(uguiComp);
-			object rtn = _ListenerList[idx];
-			_UguiCompList.RemoveAt(idx);
-			_BindableList.RemoveAt(idx);
-			_ListenerList.RemoveAt(idx);
-			return rtn;
+			bool contains = _UguiCompList.Contains(uguiComp) && _BindableList.Contains(bindableData);
+			return contains;
 		}
-		return null;
+
+		/// <summary>
+		/// Bind指定的Ugui组件和数据
+		/// </summary>
+		internal void Bind(object uguiComp, object bindableData, object listener)
+		{
+			if (!AlreadyBinded(uguiComp, bindableData))
+			{
+				_UguiCompList.Add(uguiComp);
+				_BindableList.Add(bindableData);
+				_ListenerList.Add(listener);
+			}
+		}
+
+		/// <summary>
+		/// 解除Bind指定的Ugui组件和数据
+		/// </summary>
+		internal object Unbind(object uguiComp, object bindableData)
+		{
+			if (AlreadyBinded(uguiComp, bindableData))
+			{
+				int idx = _UguiCompList.IndexOf(uguiComp);
+				object rtn = _ListenerList[idx];
+				_UguiCompList.RemoveAt(idx);
+				_BindableList.RemoveAt(idx);
+				_ListenerList.RemoveAt(idx);
+				return rtn;
+			}
+			return null;
+		}
 	}
 }
