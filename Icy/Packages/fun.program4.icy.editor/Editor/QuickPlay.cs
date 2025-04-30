baf082e9f5fa2efe786181c0d094ce31cbe8eccc
Icy <chengshengtzss@163.com>
2025-04-30 10:30:29 +0800

    Add : RePlay ediotr 扩展按钮

--- Icy/Packages/fun.program4.icy.editor/Editor/QuickPlay.cs
+++ Icy/Packages/fun.program4.icy.editor/Editor/QuickPlay.cs
@@ -5,28 +5,30 @@ using UnityToolbarExtender;
 namespace Icy.Editor
 {
 	/// <summary>
-	/// 不 进行Domain Reload，直接进入Play状态；
-	/// 使用这个功能需要知道其原理和限制，否则请不要使用这个功能
+	/// 在原生Play按钮右侧，添加快速Play、RePlay按钮，加快迭代速度
 	/// </summary>
 	[InitializeOnLoad]
 	public class QuickPlayViewer
 	{
-		private static Texture _Icon;
+		private static Texture _QuickPlayIcon;
+		private static Texture _RePlayIcon;
+		private static bool _IsRequestingRePlay;
 
 		static QuickPlayViewer()
 		{
 			ToolbarExtender.RightToolbarGUI.Add(OnToolbarGUI);
 			EditorApplication.playModeStateChanged += ClearPlayMode;
 
-			if (_Icon == null)
-				_Icon = AssetDatabase.LoadAssetAtPath<Texture>("Packages/fun.program4.icy.editor/Editor/Res/QuickIcon.png");
+			if (_QuickPlayIcon == null)
+				_QuickPlayIcon = AssetDatabase.LoadAssetAtPath<Texture>("Packages/fun.program4.icy.editor/Editor/Res/QuickIcon.png");
+			if (_RePlayIcon == null)
+				_RePlayIcon = AssetDatabase.LoadAssetAtPath<Texture>("Packages/fun.program4.icy.editor/Editor/Res/ReplayIcon.png");
+
+			_IsRequestingRePlay = false;
 		}
 
 		static void OnToolbarGUI()
 		{
-			if (EditorApplication.isPlaying)
-				return;
-
 			Color originalBackgroundColor = GUI.backgroundColor;
 			GUI.backgroundColor = new Color(180.0f / 255.0f, 180.0f / 255.0f, 180.0f / 255.0f);
 
@@ -41,12 +43,26 @@ namespace Icy.Editor
 				padding = new RectOffset(0, 0, 3, 3)
 			};
 
-			if (GUILayout.Button(new GUIContent("QuickPlay", _Icon, "不 进行Domain Reload，直接进入Play状态；\n使用这个功能需要知道其原理和限制，否则请不要使用这个功能"), style))
-				QuickPlay();
+			if (!EditorApplication.isPlaying)
+			{
+				if (GUILayout.Button(new GUIContent("QuickPlay", _QuickPlayIcon, "不 进行Domain Reload，直接进入Play状态；\n使用这个功能需要知道其原理和限制，否则请不要使用这个功能"), style))
+					QuickPlay();
+			}
+
+			if (EditorApplication.isPlaying)
+			{
+				style.fixedWidth = 70;
+				if (GUILayout.Button(new GUIContent(" RePlay", _RePlayIcon, "不 进行Domain Reload，直接进入Play状态；\n使用这个功能需要知道其原理和限制，否则请不要使用这个功能"), style))
+					RePlay();
+			}
 
 			GUI.backgroundColor = originalBackgroundColor;
 		}
 
+		/// <summary>
+		/// 不 进行Domain Reload，直接进入Play状态；
+		/// 使用这个功能需要知道其原理和限制，否则请不要使用这个功能
+		/// </summary>
 		static void QuickPlay()
 		{
 			if (EditorApplication.isPlaying == false)
@@ -54,6 +70,18 @@ namespace Icy.Editor
 				EditorSettings.enterPlayModeOptionsEnabled = true;
 				EditorSettings.enterPlayModeOptions = EnterPlayModeOptions.DisableDomainReload;
 				EditorApplication.EnterPlaymode();
+			}
+		}
+
+		/// <summary>
+		/// 退出Play状态，然后使用QuickPlay重新进入Play状态
+		/// </summary>
+		static void RePlay()
+		{
+			if (EditorApplication.isPlaying)
+			{
+				EditorApplication.ExitPlaymode();
+				_IsRequestingRePlay = true;
 			}
 		}
 
@@ -65,6 +93,15 @@ namespace Icy.Editor
 				{
 					EditorSettings.enterPlayModeOptionsEnabled = false;
 					EditorSettings.enterPlayModeOptions = EnterPlayModeOptions.None;
+				}
+			}
+
+			if (state == PlayModeStateChange.EnteredEditMode)
+			{
+				if (_IsRequestingRePlay)
+				{
+					_IsRequestingRePlay = false;
+					QuickPlay();
 				}
 			}
 		}
