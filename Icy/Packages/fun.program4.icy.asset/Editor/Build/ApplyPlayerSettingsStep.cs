964ddcd79d30aa8a7347f6c11e413afbed44e0b9
Icy <chengshengtzss@163.com>
2025-05-05 16:38:24 +0800

    Add : 打包前的PlayerSettings修改

---
+++ Icy/Packages/fun.program4.icy.asset/Editor/Build/ApplyPlayerSettingsStep.cs
@@ -0,0 +1,58 @@
+using Icy.Base;
+using Cysharp.Threading.Tasks;
+using UnityEditor;
+
+namespace Icy.Asset.Editor
+{
+	/// <summary>
+	/// 把框架Setting文件PlayerSetting部分，应用到PlayerSetting
+	/// </summary>
+	public class ApplyPlayerSettingsStep : ProcedureStep
+	{
+		private BuildTarget _BuildTarget;
+		private BuildSetting _BuildSetting;
+
+		public override async UniTask Activate()
+		{
+			_BuildTarget = (BuildTarget)_Procedure.Blackboard.ReadInt("BuildTarget");
+			_BuildSetting = _Procedure.Blackboard.ReadObject("BuildSetting") as BuildSetting;
+
+			if (_BuildSetting != null)
+			{
+				if (!string.IsNullOrEmpty(_BuildSetting.ApplicationIdentifier))
+					PlayerSettings.applicationIdentifier = _BuildSetting.ApplicationIdentifier;
+
+				if (!string.IsNullOrEmpty(_BuildSetting.ProductName))
+					PlayerSettings.productName = _BuildSetting.ProductName;
+
+				if (!string.IsNullOrEmpty(_BuildSetting.CompanyName))
+					PlayerSettings.companyName = _BuildSetting.CompanyName;
+
+				if (!string.IsNullOrEmpty(_BuildSetting.Version))
+					PlayerSettings.bundleVersion = _BuildSetting.Version;
+
+				switch (_BuildTarget)
+				{
+					case BuildTarget.Android:
+						//PlayerSettings.Android.keyaliasPass = "";
+						//PlayerSettings.Android.keystorePass = "";
+						break;
+					case BuildTarget.iOS:
+						break;
+					case BuildTarget.StandaloneWindows64:
+						break;
+					default:
+						break;
+				}
+			}
+
+			Finish();
+			await UniTask.CompletedTask;
+		}
+
+		public override async UniTask Deactivate()
+		{
+			await UniTask.CompletedTask;
+		}
+	}
+}
