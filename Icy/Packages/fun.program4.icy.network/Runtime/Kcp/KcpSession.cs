da6ee10d45ece4ce8de1798efa8956f2c4919d91
Icy <chengshengtzss@163.com>
2025-06-06 10:17:37 +0800

    Add : Native版的KCP

--- Icy/Packages/fun.program4.icy.network/Runtime/Kcp/KcpSession.cs
+++ Icy/Packages/fun.program4.icy.network/Runtime/Kcp/KcpSession.cs
@@ -1,8 +1,13 @@
+//#define USE_KCP_SHARP //定义的话，使用C#版的KCP，否则使用Native版的；这里仅做测试用，正式开启去PlayerSetting中设置Symbol
+
 using Cysharp.Threading.Tasks;
 using Icy.Base;
 using System;
 using System.Net;
 using System.Net.Sockets;
+#if !USE_KCP_SHARP
+using System.Runtime.InteropServices;
+#endif
 
 namespace Icy.Network
 {
@@ -33,10 +38,17 @@ namespace Icy.Network
 		/// 底层UDP Socket
 		/// </summary>
 		protected Socket _Socket;
+#if USE_KCP_SHARP
 		/// <summary>
 		/// C#版本的Kcp
 		/// </summary>
 		protected Kcp _Kcp;
+#else
+		/// <summary>
+		/// Native版本的Kcp
+		/// </summary>
+		protected IntPtr _Kcp = IntPtr.Zero;
+#endif
 		/// <summary>
 		/// 服务器地址和端口
 		/// </summary>
@@ -49,6 +61,14 @@ namespace Icy.Network
 		/// 用于接收的EndPoint
 		/// </summary>
 		protected EndPoint _IpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+		/// <summary>
+		/// 当前错误码
+		/// </summary>
+		protected int _Error;
+
+#if !USE_KCP_SHARP && !ENABLE_IL2CPP
+		protected KcpOutput _KcpOutput;
+#endif
 
 		protected uint _LocalConn;
 		protected uint _RemoteConn;
@@ -71,20 +91,35 @@ namespace Icy.Network
 			_LocalConn = (uint)UnityEngine.Random.Range(1000, int.MaxValue);
 			_RemoteConn = _LocalConn;
 
+#if USE_KCP_SHARP
 			_Kcp = new Kcp(_RemoteConn, new IntPtr(_LocalConn));
 			_Kcp.SetOutput(KcpOutput);
 
 			_Kcp.NoDelay(1, 10, 1, 1);
 			_Kcp.WndSize(32, 32);
 			_Kcp.SetMTU(470);
+#else
+			_Kcp = KcpDll.KcpCreate(_RemoteConn, new IntPtr(_LocalConn));
+			SetOutput();
+
+			KcpDll.KcpNodelay(_Kcp, 1, 10, 1, 1);
+			KcpDll.KcpWndsize(_Kcp, 32, 32);
+			KcpDll.KcpSetmtu(_Kcp, 470);
+#endif
 
 			_LastRecvTime = _TimeNow;
 
 			try
 			{
+#if USE_KCP_SHARP
 				_SendBuffer.WriteTo(0, 4);
 				_SendBuffer.WriteTo(4, KcpProtocolType.SYN);
 				Send(_SendBuffer, 0, 6);
+#else
+				_SendBuffer.WriteTo(0, KcpProtocolType.SYN);
+				_SendBuffer.WriteTo(2, _LocalConn);
+				_Socket.SendTo(_SendBuffer, 0, 6, SocketFlags.None, _RemoteEndPoint);
+#endif
 			}
 			catch (Exception e)
 			{
@@ -94,6 +129,162 @@ namespace Icy.Network
 			return UniTask.CompletedTask;
 		}
 
+		public override UniTask Listen()
+		{
+			return UniTask.CompletedTask;
+		}
+
+#if !USE_KCP_SHARP
+		public void SetOutput()
+		{
+#if ENABLE_IL2CPP
+			KcpDll.KcpSetoutput(_Kcp, KcpOutput);
+#else
+			// 跟上一行一样写法，pc跟linux会出错, 保存防止被GC
+			_KcpOutput = KcpOutput;
+			KcpDll.KcpSetoutput(_Kcp, _KcpOutput);
+#endif
+		}
+#endif
+
+		public override void Send(byte[] msg, int startIdx, int length)
+		{
+#if USE_KCP_SHARP
+			if (_Kcp == null)
+#else
+			if (_Kcp == IntPtr.Zero)
+#endif
+				OnListenException?.Invoke(new Exception($"Call {nameof(Send)} when Kcp is disconnected"));
+			else
+			{
+#if USE_KCP_SHARP
+				_Kcp.Send(msg, startIdx, length);
+#else
+				KcpDll.KcpSend(_Kcp, msg, length);
+#endif
+			}
+		}
+
+#if USE_KCP_SHARP
+		protected void KcpOutput(byte[] bytes, int len, object user)
+		{
+			try
+			{
+				if (len == 0)
+				{
+					Log.LogError($"Kcp output 0", nameof(KcpSession));
+					return;
+				}
+
+				Buffer.BlockCopy(bytes, 0, _SendBuffer, 0, len);
+				_Socket.SendTo(_SendBuffer, 0, len, SocketFlags.None, _RemoteEndPoint);
+			}
+			catch (Exception e)
+			{
+				OnListenException(e);
+			}
+		}
+#else
+#if ENABLE_IL2CPP
+		[AOT.MonoPInvokeCallback(typeof(KcpOutput))]
+#endif
+		protected int KcpOutput(IntPtr bytes, int len, IntPtr kcp, IntPtr user)
+		{
+			try
+			{
+				if (len == 0)
+				{
+					Log.LogError($"Kcp output 0", nameof(KcpSession));
+					return 0;
+				}
+
+				Marshal.Copy(bytes, _SendBuffer, 0, len);
+				_Socket.SendTo(_SendBuffer, 0, len, SocketFlags.None, _RemoteEndPoint);
+			}
+			catch (Exception e)
+			{
+				OnListenException(e);
+			}
+			return len;
+		}
+#endif
+
+		protected virtual void Recv()
+		{
+			while (_Socket != null && _Socket.Available > 0)
+			{
+				int messageLength = 0;
+				try
+				{
+					messageLength = _Socket.ReceiveFrom(_ReceiveBuffer, ref _IpEndPoint);
+					if (messageLength < 1)
+						continue;
+
+					HandleReceived(_ReceiveBuffer, messageLength);
+				}
+				catch (Exception e)
+				{
+					OnListenException?.Invoke(e);
+				}
+			}
+		}
+
+		protected override void HandleReceived(byte[] buffer, int receivedSize)
+		{
+#if USE_KCP_SHARP
+			_Kcp.Input(buffer, 0, receivedSize);
+#else
+			KcpDll.KcpInput(_Kcp, buffer, 0, receivedSize);
+#endif
+
+			while (true)
+			{
+#if USE_KCP_SHARP
+				int n = _Kcp.PeekSize();
+#else
+				int n = KcpDll.KcpPeeksize(_Kcp);
+#endif
+				if (n < 0)
+					return;
+
+				if (n == 0)
+				{
+					OnListenException(new Exception());
+					return;
+				}
+
+#if USE_KCP_SHARP
+				int count = _Kcp.Recv(buffer, 0, ushort.MaxValue);
+#else
+				int count = KcpDll.KcpRecv(_Kcp, buffer, ushort.MaxValue);
+#endif
+
+				if (n != count)
+					return;
+
+				if (count <= 0)
+					return;
+
+				_LastRecvTime = _TimeNow;
+
+				if (!IsConnected)
+				{
+					IsConnected = true;
+					OnConnected?.Invoke();
+				}
+				else
+					OnReceive?.Invoke(buffer, 0, n);
+			}
+		}
+
+		/// <summary>
+		/// 客户端当前时间，单位ms
+		/// </summary>
+		protected long ClientNow()
+		{
+			return (DateTime.UtcNow.Ticks - epoch) / 10000;
+		}
+
 		public override void Disconnect()
 		{
 			if (_Socket == null)
@@ -101,6 +292,7 @@ namespace Icy.Network
 
 			try
 			{
+#if USE_KCP_SHARP
 				_SendBuffer.WriteTo(0, 4);
 				_SendBuffer.WriteTo(4, KcpProtocolType.FIN);
 				Send(_SendBuffer, 0, 6);
@@ -110,6 +302,19 @@ namespace Icy.Network
 					_Kcp.Release();
 					_Kcp = null;
 				}
+#else
+				_SendBuffer.WriteTo(0, KcpProtocolType.FIN);
+				_SendBuffer.WriteTo(2, _LocalConn);
+				_SendBuffer.WriteTo(6, _RemoteConn);
+				_SendBuffer.WriteTo(10, (uint)_Error);
+				_Socket.SendTo(_SendBuffer, 0, 14, SocketFlags.None, _RemoteEndPoint);
+
+				if (_Kcp != IntPtr.Zero)
+				{
+					KcpDll.KcpRelease(_Kcp);
+					_Kcp = IntPtr.Zero;
+				}
+#endif
 				_Socket = null;
 
 				IsConnected = false;
@@ -141,106 +346,15 @@ namespace Icy.Network
 			Log.LogInfo("Dispose", nameof(KcpSession));
 		}
 
-		public override UniTask Listen()
-		{
-			return UniTask.CompletedTask;
-		}
-
-		public override void Send(byte[] msg, int startIdx, int length)
-		{
-			if (_Kcp == null)
-				OnListenException?.Invoke(new Exception($"Call {nameof(Send)} when Kcp is disconnected"));
-			else
-				_Kcp.Send(msg, startIdx, length);
-		}
-
-		protected virtual void Recv()
-		{
-			while (_Socket != null && _Socket.Available > 0)
-			{
-				int messageLength = 0;
-				try
-				{
-					messageLength = _Socket.ReceiveFrom(_ReceiveBuffer, ref _IpEndPoint);
-					if (messageLength < 1)
-						continue;
-
-					HandleReceived(_ReceiveBuffer, messageLength);
-				}
-				catch (Exception e)
-				{
-					OnListenException?.Invoke(e);
-				}
-			}
-		}
-
-		protected override void HandleReceived(byte[] buffer, int receivedSize)
-		{
-			_Kcp.Input(buffer, 0, receivedSize);
-
-			while (true)
-			{
-				int n = _Kcp.PeekSize();
-				if (n < 0)
-					return;
-
-				if (n == 0)
-				{
-					OnListenException(new Exception());
-					return;
-				}
-
-				int count = _Kcp.Recv(buffer, 0, ushort.MaxValue);
-
-				if (n != count)
-					return;
-
-				if (count <= 0)
-					return;
-
-				_LastRecvTime = _TimeNow;
-
-				if (!IsConnected)
-				{
-					IsConnected = true;
-					OnConnected?.Invoke();
-				}
-				else
-					OnReceive?.Invoke(buffer, 0, n);
-			}
-		}
-
-		protected void KcpOutput(byte[] bytes, int len, object user)
-		{
-			try
-			{
-				if (len == 0)
-				{
-					Log.LogError($"Kcp output 0", nameof(KcpSession));
-					return;
-				}
-
-				Buffer.BlockCopy(bytes, 0, _SendBuffer, 0, len);
-				_Socket.SendTo(_SendBuffer, 0, len, SocketFlags.None, _RemoteEndPoint);
-			}
-			catch (Exception e)
-			{
-				OnListenException(e);
-			}
-		}
-
-		/// <summary>
-		/// 客户端当前时间，单位ms
-		/// </summary>
-		protected long ClientNow()
-		{
-			return (DateTime.UtcNow.Ticks - epoch) / 10000;
-		}
-
 		public void Update(float delta)
 		{
+#if USE_KCP_SHARP
 			if (_Kcp == null)
 				return;
+#else
+			if (_Kcp == IntPtr.Zero)
+				return;
+#endif
 
 			_TimeNow = (uint)(ClientNow() - _StartTime);
 
@@ -248,7 +362,11 @@ namespace Icy.Network
 
 			try
 			{
+#if USE_KCP_SHARP
 				_Kcp.Update(_TimeNow);
+#else
+				KcpDll.KcpUpdate(_Kcp, _TimeNow);
+#endif
 			}
 			catch (Exception e)
 			{
@@ -258,7 +376,11 @@ namespace Icy.Network
 
 			if (_Kcp != null)
 			{
+#if USE_KCP_SHARP
 				uint nextUpdateTime = _Kcp.Check(_TimeNow);
+#else
+				uint nextUpdateTime = KcpDll.KcpCheck(_Kcp, _TimeNow);
+#endif
 			}
 		}
 	}
