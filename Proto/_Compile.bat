a68c0a0636756adbeb2b5262e700957d77f1f542
Icy <chengshengtzss@163.com>
2025-06-18 20:20:59 +0800

    重新生成Proto、清空输出目录时，保留ResetMethodExtension.cs文件

--- Proto/_Compile.bat
+++ Proto/_Compile.bat
@@ -6,8 +6,9 @@ chcp  65001
 
 :: 输出目录
 set "target_dir=../Icy/Assets/Example/Protos"
-:: asmdef文件要保留
+:: asmdef和Reset Extension文件要保留
 set "keep_file=Protos.asmdef"
+set "keep_file2=ResetMethodExtension.cs"
 set "keep_extension=.meta"
 :: 记录当前目录
 set "cur_dir=%~dp0"
@@ -17,8 +18,10 @@ cd /d "%target_dir%"
 :: 删除除指定文件外的所有文件
 for %%f in (*.*) do (
     if not "%%f"=="%keep_file%" (
-        if not "%%~xf"=="%keep_extension%" (
-            del /q "%%f"
+        if not "%%f"=="%keep_file2%" (
+            if not "%%~xf"=="%keep_extension%" (
+                del /q "%%f"
+            )
         )
     )
 )
@@ -28,7 +31,6 @@ for /d %%d in (*) do (
     rd /s /q "%%d"
 )
 
-::echo 目录已清空，保留了文件 %keep_file% 和 所有后缀为 %keep_extension% 的文件
 cd /d "%cur_dir%"
 
 
