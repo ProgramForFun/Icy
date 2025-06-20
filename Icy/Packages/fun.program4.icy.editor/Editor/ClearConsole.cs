4608a6a7987a3526de023edecc3eb7c662a34bae
Icy <chengshengtzss@163.com>
2025-06-20 10:42:54 +0800

    Add : 清空Unity editor的Console窗口的Icy菜单选项

---
+++ Icy/Packages/fun.program4.icy.editor/Editor/ClearConsole.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Reflection;
+using UnityEditor;
+
+/// <summary>
+/// 用反射调用内部类，清空Unity editor的Console窗口
+/// </summary>
+public static class ClearConsole
+{
+	[MenuItem("Icy/Tools/Clear Console", false, 20)]
+	public static void Clear()
+	{
+		Type logEntries = Type.GetType("UnityEditor.LogEntries, UnityEditor.dll");
+		MethodInfo clearMethod = logEntries.GetMethod("Clear", BindingFlags.Static | BindingFlags.Public);
+		clearMethod?.Invoke(null, null);
+	}
+}
