00bb502a41b37dc6120fdf11b3eca08bf6c54d3b
Icy <chengshengtzss@163.com>
2025-07-28 21:04:39 +0800

    Add : KCP Echo服务器

---
+++ Icy/Packages/fun.program4.icy.network/Runtime/Test/KcpEcho.py
@@ -0,0 +1,34 @@
+# pip install kcp
+
+from kcp.server import Connection
+from kcp.server import KCPServerAsync
+
+# Create the initial server instance.
+server = KCPServerAsync(
+    "127.0.0.1",
+    12333,
+    conv_id=1,
+    no_delay=True,
+)
+
+# Ability to set performance options after initialisation.
+server.set_performance_options(
+    update_interval=10,
+)
+
+
+# Ran when the server starts.
+@server.on_start
+async def on_start() -> None:
+    print("Server started!")
+
+
+# Ran when a connection is made.
+@server.on_data
+async def on_data(connection: Connection, data: bytes) -> None:
+    print(type(data))
+    print(f"Received data from {connection.address}: {data}")
+    connection.enqueue(bytes(data))
+
+
+server.start()
