4c08dc5f97c94fbb349fc348147c754f2111ceb5
Icy <chengshengtzss@163.com>
2025-08-10 19:19:26 +0800

    Add : 支持显示SVN分支

--- Icy/Packages/fun.program4.icy.editor/Editor/BranchDisplaying.cs
+++ Icy/Packages/fun.program4.icy.editor/Editor/BranchDisplaying.cs
@@ -17,6 +17,8 @@
 
 using Icy.Base;
 using System;
+using System.Diagnostics;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 using UnityToolbarExtender;
@@ -24,10 +26,12 @@ using UnityToolbarExtender;
 namespace Icy.Editor
 {
 	/// <summary>
-	/// 在Play按钮那一行的最左侧，显示Git分支名，方便区分分支
+	/// 在Play按钮那一行的最左侧，显示版本控制的分支名，方便区分分支
 	/// </summary>
 	public class BranchDisplaying
 	{
+		private const string GET_REPO_TYPE_KEY = "_Icy_GetRepo";
+		private static RepositoryType _RepoType;
 		private static string _BranchName;
 		private static long _PrevUpdateBranchNameTimestamp;
 
@@ -35,6 +39,19 @@ namespace Icy.Editor
 		static void Init()
 		{
 			_PrevUpdateBranchNameTimestamp = 0;
+
+			//每次启动Unity，只执行一次判断Repo类型
+			if (SessionState.GetString(GET_REPO_TYPE_KEY, string.Empty) == string.Empty)
+			{
+				_RepoType = DetectRepository();
+				SessionState.SetString(GET_REPO_TYPE_KEY, GET_REPO_TYPE_KEY);
+
+				EditorLocalPrefs.SetInt(GET_REPO_TYPE_KEY, (int)_RepoType);
+				EditorLocalPrefs.Save();
+			}
+
+			_RepoType = (RepositoryType)EditorLocalPrefs.GetInt(GET_REPO_TYPE_KEY, 0);
+
 			ToolbarExtender.LeftToolbarGUI.Add(OnToolbarGUI);
 		}
 
@@ -48,14 +65,27 @@ namespace Icy.Editor
 			if (totalSeconds - _PrevUpdateBranchNameTimestamp > 10)
 			{
 				_PrevUpdateBranchNameTimestamp = totalSeconds;
-				_BranchName = GetGitBranch();
+				switch (_RepoType)
+				{
+					case RepositoryType.Git:
+						_BranchName = GetGitBranch();
+						break;
+					case RepositoryType.SVN:
+						_BranchName = GetSvnBranch();
+						break;
+				}
 			}
-			GUILayout.Label($"Git Branch  :  {_BranchName}", style);
+
+			if (_RepoType != RepositoryType.NotSet && _RepoType != RepositoryType.None)
+				GUILayout.Label($"{_RepoType} Branch  :  {_BranchName}", style);
 		}
 
-			static string GetGitBranch()
+		/// <summary>
+		/// 获取Git分支名
+		/// </summary>
+		static string GetGitBranch()
 		{
-			System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo
+			ProcessStartInfo startInfo = new ProcessStartInfo
 			{
 				FileName = "git",
 				Arguments = "rev-parse --abbrev-ref HEAD",
@@ -64,14 +94,76 @@ namespace Icy.Editor
 				CreateNoWindow = true
 			};
 
-			using (System.Diagnostics.Process process = System.Diagnostics.Process.Start(startInfo))
+			using (Process process = Process.Start(startInfo))
 			{
-				using (System.IO.StreamReader reader = process.StandardOutput)
+				using (StreamReader reader = process.StandardOutput)
 				{
 					string result = reader.ReadToEnd().Trim();
 					return result;
 				}
 			}
+		}
+
+		/// <summary>
+		/// 获取SVN分支名
+		/// </summary>
+		static string GetSvnBranch()
+		{
+			var process = new Process
+			{
+				StartInfo = new ProcessStartInfo
+				{
+					FileName = "svn",
+					Arguments = "info --show-item relative-url",
+					RedirectStandardOutput = true,
+					UseShellExecute = false,
+					CreateNoWindow = true
+				}
+			};
+
+			process.Start();
+			string output = process.StandardOutput.ReadToEnd();
+			process.WaitForExit();
+
+			if (process.ExitCode == 0)
+			{
+				string relativeUrl = output.Trim().ToLower();
+				// 示例格式： ^/branches/your-branch-name
+				if (relativeUrl.Contains("/branches/"))
+					return relativeUrl.Split(new[] { "/branches/" }, System.StringSplitOptions.None)[1].Split('/')[0];
+				return relativeUrl.Contains("/trunk") ? "trunk" : string.Empty;
+			}
+			return string.Empty;
+		}
+
+		/// <summary>
+		/// 获取当前工作目录是Git仓库，还是SVN仓库
+		/// </summary>
+		static RepositoryType DetectRepository(string path = null)
+		{
+			path ??= Directory.GetCurrentDirectory();
+			var current = new DirectoryInfo(path);
+
+			while (current != null)
+			{
+				if (Directory.Exists(Path.Combine(current.FullName, ".git")))
+					return RepositoryType.Git;
+
+				if (Directory.Exists(Path.Combine(current.FullName, ".svn")))
+					return RepositoryType.SVN;
+
+				current = current.Parent; // 向上一级目录查找
+			}
+
+			return RepositoryType.None;
+		}
+
+		public enum RepositoryType
+		{
+			NotSet,	//初始值
+			None,	//不在任何版本控制目录中
+			Git,		//在Git中
+			SVN,		//在SVN中
 		}
 	}
 }
