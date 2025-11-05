d63d0824e1bc6db7abe0d98dc06b56920c99584f
Icy <chengshengtzss@163.com>
2025-11-05 10:47:14 +0800

    SetColorOnce从worker线程调用的提示

--- Icy/Packages/fun.program4.icy.base/Runtime/Log/Log.cs
+++ Icy/Packages/fun.program4.icy.base/Runtime/Log/Log.cs
@@ -196,6 +196,12 @@ namespace Icy.Base
 		public static void SetColorOnce(Color color)
 		{
 #if UNITY_EDITOR
+			if (!IcyFrame.Instance.IsMainThread())
+			{
+				Error("Call Log.SetColorOnce from worker thread is not supported now", nameof(SetColorOnce));
+				return;
+			}
+
 			_ColorOnce = ColorUtility.ToHtmlStringRGB(color);
 #endif
 		}
