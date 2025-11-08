f92a0910c5d31628cf0225882a2ddbf4e0916675
Icy <chengshengtzss@163.com>
2025-11-08 13:01:13 +0800

    使注释更明确

--- Icy/Packages/fun.program4.icy.base/Runtime/IcyFrame.cs
+++ Icy/Packages/fun.program4.icy.base/Runtime/IcyFrame.cs
@@ -54,7 +54,7 @@ namespace Icy.Base
 
 #if ICY_PRESERVE_UNITY_CLASS
 			int dummy = UnityEngine.Random.Range(1, 2);
-			//只保证代码有引用、不被裁剪，不会执行
+			//只保证代码有引用、不被裁剪，实际不会执行到
 			if (dummy == 0)
 				UnityClassReferencer.Preserve();
 #endif
