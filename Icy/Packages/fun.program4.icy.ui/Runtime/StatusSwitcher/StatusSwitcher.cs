67bced2cb03d631030bb95c651a74bf75d3207bd
Icy <chengshengtzss@163.com>
2025-12-28 22:27:46 +0800

    StatusSwitcher 的 Inspector 表现

--- Icy/Packages/fun.program4.icy.ui/Runtime/StatusSwitcher/StatusSwitcher.cs
+++ Icy/Packages/fun.program4.icy.ui/Runtime/StatusSwitcher/StatusSwitcher.cs
@@ -16,18 +16,29 @@
 
 
 using Icy.Base;
-using System.Collections;
+using Sirenix.OdinInspector;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Icy.UI
 {
+	[HideMonoScript]
 	public class StatusSwitcher : MonoBehaviour
 	{
 		protected List<StatusSwitcherTarget> _SwitcherTargetList = new List<StatusSwitcherTarget>();
 
+		/// <summary>
+		/// 所有的Status
+		/// </summary>
+		[ListDrawerSettings(ShowItemCount = true, DraggableItems = true, ShowFoldout = false, HideAddButton = true)]
 		[SerializeField]
-		protected List<StatusName> _Statuses = new List<StatusName>();
+		protected List<StatusName> _StatusList = new List<StatusName>();
+
+		/// <summary>
+		/// 当前在Dirty状态的Status
+		/// </summary>
+		internal string CurrDirtyStatus;
 
 
 		/// <summary>
@@ -35,9 +46,9 @@ namespace Icy.UI
 		/// </summary>
 		public void SwitchTo(string statusName)
 		{
-			for (int i = 0; i < _Statuses.Count; i++)
+			for (int i = 0; i < _StatusList.Count; i++)
 			{
-				if (_Statuses[i].Name == statusName)
+				if (_StatusList[i].Name == statusName)
 				{
 
 					return;
@@ -47,9 +58,22 @@ namespace Icy.UI
 		}
 
 #if UNITY_EDITOR
-		internal void AddNewStatus(string statusName)
-		{
+		[HorizontalGroup("Add")]
+		[ShowInInspector]
+		[HideLabel]
+		protected string _InputName;
 
+		[EnableIf(nameof(IsValidName))]
+		[HorizontalGroup("Add")]
+		[Button("Add", ButtonSizes.Medium, Icon = SdfIconType.PlusCircleFill)]
+		protected void AddNewStatus()
+		{
+			_StatusList.Add(new StatusName(this, _InputName));
+		}
+
+		protected bool IsValidName()
+		{
+			return !string.IsNullOrEmpty(_InputName);
 		}
 
 		internal void DeleteStatus(string statusName)
@@ -61,21 +85,81 @@ namespace Icy.UI
 		{
 
 		}
-
-		internal void Save()
-		{
-
-		}
 #endif
 	}
 
-	[System.Serializable]
+	[Serializable]
 	public class StatusName
 	{
+		/// <summary>
+		/// 状态名字
+		/// </summary>
+		[HideInInspector]
 		[SerializeField]
 		public string Name;
 
+		/// <summary>
+		/// 所属StatusSwitcher
+		/// </summary>
+		[HideInInspector]
 		[SerializeField]
-		public bool IsDirty = true;
+		protected StatusSwitcher _StatusSwitcher;
+
+		/// <summary>
+		/// 状态是否在编辑中
+		/// </summary>
+		[NonSerialized]
+		protected bool _IsDirty = false;
+
+		protected string _DisplayName => _IsDirty? Name + "（Editing...）" : Name;
+
+		public StatusName(StatusSwitcher statusSwitcher, string name)
+		{
+			_StatusSwitcher = statusSwitcher;
+			Name = name;
+		}
+
+		/// <summary>
+		/// 状态按钮本体
+		/// </summary>
+		[HorizontalGroup("H")]
+		[Button("$_DisplayName", ButtonSizes.Medium)]
+		protected void StatusList()
+		{
+			Debug.Log($"点击了Status: {Name}");
+		}
+
+		/// <summary>
+		/// 编辑按钮
+		/// </summary>
+		[HideIf(nameof(_IsDirty))]
+		[HorizontalGroup("H", 50)]
+		[Button("Edit", ButtonSizes.Medium)]
+		protected void Edit()
+		{
+			if (!string.IsNullOrEmpty(_StatusSwitcher.CurrDirtyStatus))
+			{
+				string msg = $"先保存{_StatusSwitcher.CurrDirtyStatus}后，再尝试编辑其他状态";
+				CommonUtility.SafeDisplayDialog("", msg, "OK", LogLevel.Warning);
+				return;
+			}
+
+			Debug.Log($"Edit: {Name}");
+			_StatusSwitcher.CurrDirtyStatus = Name;
+			_IsDirty = true;
+		}
+
+		/// <summary>
+		/// 保存按钮
+		/// </summary>
+		[ShowIf(nameof(_IsDirty))]
+		[HorizontalGroup("H", 50)]
+		[Button("Save", ButtonSizes.Medium), GUIColor(0, 1, 0)]
+		protected void Save()
+		{
+			Debug.Log($"Save: {Name}");
+			_StatusSwitcher.CurrDirtyStatus = null;
+			_IsDirty = false;
+		}
 	}
 }
