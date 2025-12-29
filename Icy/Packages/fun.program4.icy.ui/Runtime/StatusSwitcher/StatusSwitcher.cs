46809dcd1c78599635363075498e0a7673acf145
Icy <chengshengtzss@163.com>
2025-12-29 19:27:30 +0800

    StatusSwitcher 的 Inspector 表现，Target部分

--- Icy/Packages/fun.program4.icy.ui/Runtime/StatusSwitcher/StatusSwitcher.cs
+++ Icy/Packages/fun.program4.icy.ui/Runtime/StatusSwitcher/StatusSwitcher.cs
@@ -26,19 +26,128 @@ namespace Icy.UI
 	[HideMonoScript]
 	public class StatusSwitcher : MonoBehaviour
 	{
-		protected List<StatusSwitcherTarget> _SwitcherTargetList = new List<StatusSwitcherTarget>();
-
 		/// <summary>
 		/// 所有的Status
 		/// </summary>
+		[BoxGroup("状态列表")]
 		[ListDrawerSettings(ShowItemCount = true, DraggableItems = true, ShowFoldout = false, HideAddButton = true)]
 		[SerializeField]
 		protected List<StatusName> _StatusList = new List<StatusName>();
 
+		//新Status的名字
+		[BoxGroup("状态列表")]
+		[HorizontalGroup("状态列表/Add")]
+		[ShowInInspector]
+		[HideLabel]
+		[NonSerialized]
+		protected string _InputName;
+
+		//点击添加新Status的按钮
+		[PropertySpace(0, 20)]
+		[BoxGroup("状态列表")]
+		[EnableIf(nameof(IsValidName))]
+		[HorizontalGroup("状态列表/Add")]
+		[Button("Add", ButtonSizes.Medium, Icon = SdfIconType.PlusCircleFill)]
+		protected void AddNewStatus()
+		{
+			for (int i = 0; i < _StatusList.Count; i++)
+			{
+				if (_StatusList[i].Name == _InputName)
+				{
+					string msg = $"重复的Status名字：{_InputName}";
+					CommonUtility.SafeDisplayDialog("", msg, "OK", LogLevel.Error);
+					return;
+				}
+			}
+
+			_StatusList.Add(new StatusName(this, _InputName));
+			_InputName = null;
+		}
+
+		//控制的所有节点
+		[ShowIf(nameof(NeedShowTargetList))]
+		[FoldoutGroup("控制的节点")]
+		[ListDrawerSettings(ShowItemCount = true, DraggableItems = true, ShowFoldout = false, DefaultExpandedState = false, HideAddButton = true)]
+		[SerializeField]
+		internal List<StatusSwitcherTarget> SwitcherTargetList;
+
+		[ShowIf(nameof(NeedShowTargetList))]
+		[FoldoutGroup("控制的节点")]
+		[Button("Add", ButtonSizes.Medium, Icon = SdfIconType.PlusCircleFill)]
+		protected void AddNewTarget()
+		{
+#if UNITY_EDITOR
+			UnityEditor.EditorApplication.update += OnEditorUpdate;
+			UnityEditor.EditorGUIUtility.ShowObjectPicker<StatusSwitcherTarget>(null, true, "", 666);
+#endif
+		}
+
+#if UNITY_EDITOR
+		[UnityEditor.InitializeOnLoadMethod]
+		static void Init()
+		{
+			UnityEditor.SceneManagement.PrefabStage.prefabStageClosing -= OnPrefabStageClosing;
+			UnityEditor.SceneManagement.PrefabStage.prefabStageClosing += OnPrefabStageClosing;
+		}
+
+		private static void OnPrefabStageClosing(UnityEditor.SceneManagement.PrefabStage stage)
+		{
+			if (stage.prefabContentsRoot != null)
+			{
+				StatusSwitcher[] allStatuses = stage.prefabContentsRoot.GetComponentsInChildren<StatusSwitcher>();
+				for (int i = 0; i < allStatuses.Length; i++)
+				{
+					if (allStatuses[i].CurrDirtyStatus != null)
+					{
+						string msg = $"有Status未保存：{allStatuses[i].CurrDirtyStatus.Name}";
+						CommonUtility.SafeDisplayDialog("", msg, "OK", LogLevel.Error);
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// 选择要添加的StatusSwitcherTarget
+		/// </summary>
+		private void OnEditorUpdate()
+		{
+			int pickerControlID = UnityEditor.EditorGUIUtility.GetObjectPickerControlID();
+
+			if (pickerControlID == 666)
+			{
+				UnityEngine.Object obj = UnityEditor.EditorGUIUtility.GetObjectPickerObject();
+				_CurrSelectTarget = (obj as GameObject).GetComponent<StatusSwitcherTarget>();
+			}
+
+			if (pickerControlID == 0)
+			{
+				if (_CurrSelectTarget == null)
+					SwitcherTargetList.Add(null);
+				else
+				{
+					SwitcherTargetList.Add(_CurrSelectTarget);
+				}
+
+				UnityEditor.EditorApplication.update -= OnEditorUpdate;
+			}
+		}
+#endif
+
+		private bool NeedShowTargetList()
+		{
+			return CurrDirtyStatus != null;
+		}
+
 		/// <summary>
 		/// 当前在Dirty状态的Status
 		/// </summary>
-		internal string CurrDirtyStatus;
+		[NonSerialized]
+		internal StatusName CurrDirtyStatus;
+		/// <summary>
+		/// 当前ObjectPicker窗口里选中的Target
+		/// </summary>
+		[NonSerialized]
+		protected StatusSwitcherTarget _CurrSelectTarget;
 
 
 		/// <summary>
@@ -57,20 +166,6 @@ namespace Icy.UI
 			Log.Error($"Status switching failed, {gameObject.name} has NO status called {statusName}", nameof(StatusSwitcher));
 		}
 
-#if UNITY_EDITOR
-		[HorizontalGroup("Add")]
-		[ShowInInspector]
-		[HideLabel]
-		protected string _InputName;
-
-		[EnableIf(nameof(IsValidName))]
-		[HorizontalGroup("Add")]
-		[Button("Add", ButtonSizes.Medium, Icon = SdfIconType.PlusCircleFill)]
-		protected void AddNewStatus()
-		{
-			_StatusList.Add(new StatusName(this, _InputName));
-		}
-
 		protected bool IsValidName()
 		{
 			return !string.IsNullOrEmpty(_InputName);
@@ -85,7 +180,6 @@ namespace Icy.UI
 		{
 
 		}
-#endif
 	}
 
 	[Serializable]
@@ -97,6 +191,13 @@ namespace Icy.UI
 		[HideInInspector]
 		[SerializeField]
 		public string Name;
+
+		/// <summary>
+		/// 受此状态控制的Target列表
+		/// </summary>
+		[HideInInspector]
+		[SerializeField]
+		public List<StatusSwitcherTarget> Targets;
 
 		/// <summary>
 		/// 所属StatusSwitcher
@@ -126,7 +227,7 @@ namespace Icy.UI
 		[Button("$_DisplayName", ButtonSizes.Medium)]
 		protected void StatusList()
 		{
-			Debug.Log($"点击了Status: {Name}");
+
 		}
 
 		/// <summary>
@@ -137,15 +238,18 @@ namespace Icy.UI
 		[Button("Edit", ButtonSizes.Medium)]
 		protected void Edit()
 		{
-			if (!string.IsNullOrEmpty(_StatusSwitcher.CurrDirtyStatus))
+			if (_StatusSwitcher.CurrDirtyStatus != null)
 			{
 				string msg = $"先保存{_StatusSwitcher.CurrDirtyStatus}后，再尝试编辑其他状态";
-				CommonUtility.SafeDisplayDialog("", msg, "OK", LogLevel.Warning);
+				CommonUtility.SafeDisplayDialog("", msg, "OK", LogLevel.Error);
 				return;
 			}
 
-			Debug.Log($"Edit: {Name}");
-			_StatusSwitcher.CurrDirtyStatus = Name;
+			_StatusSwitcher.CurrDirtyStatus = this;
+			if (Targets != null)
+				_StatusSwitcher.SwitcherTargetList = Targets;
+			else
+				_StatusSwitcher.SwitcherTargetList = new List<StatusSwitcherTarget>();
 			_IsDirty = true;
 		}
 
@@ -157,7 +261,8 @@ namespace Icy.UI
 		[Button("Save", ButtonSizes.Medium), GUIColor(0, 1, 0)]
 		protected void Save()
 		{
-			Debug.Log($"Save: {Name}");
+			Targets = _StatusSwitcher.SwitcherTargetList;
+			_StatusSwitcher.SwitcherTargetList = null;
 			_StatusSwitcher.CurrDirtyStatus = null;
 			_IsDirty = false;
 		}
